6649b0c91b34043eb35f6f9c1f15ef5a97cd2b42
Richard Purves <franton@users.noreply.github.com>
2016-07-07 14:30:22 +0200

    Update model_info.sh

--- model_info.sh
+++ model_info.sh
@@ -3,5 +3,5 @@
 Model=$( system_profiler SPHardwareDataType | grep "Model Identifier:" | cut -d":" -f2 )
 
 echo "hitp-enabled: YES"
-echo "hitp-title: RAM:$TotalRam"
+echo "hitp-title: Model:$Model"
 echo "hitp-state: none"
