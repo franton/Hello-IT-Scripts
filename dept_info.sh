262b546dbc0cdab5b139362b1d1814a19ca45c00
Richard Purves <franton@users.noreply.github.com>
2016-07-08 08:34:28 +0200

    Update dept_info.sh
    Reworked to read from a locally stored file rather than interrogate the API.

--- dept_info.sh
+++ dept_info.sh
@@ -2,33 +2,19 @@
 
 # Script to report current JSS department to Hello-IT
 
-# Set up variables here
-
-apiuser="apiusr"
-apipass="apiusrpw"
+deptfile="/usr/local/cs/misc/department"
 
 echo "hitp-enabled: YES"
 echo "hitp-hidden: NO"
 
-jssurl=$( defaults read /Library/Preferences/com.jamfsoftware.jamf "jss_url" 2> /dev/null )
-
-if [ "$jssurl" = "" ];
+if [ -e "$deptfile" ];
 then
-	echo "hitp-title: Not Enrolled"
-	echo "hitp-state: error"
-	exit 1
-fi
-
-department=$(curl -H "Accept: application/xml" -s -u "$apiuser:$apipass" "${jssurl}JSSResource/computers/udid/$udid/subset/location" | xpath "//computer/location/department/text()" 2> /dev/null)
-
-if [ "$department" = "" ];
-then
+	department=$( cat "$deptfile" )
+	echo "hitp-title: Department : $department"
+	echo "hitp-state: ok"
+else
 	echo "hitp-title: Missing Department Info"
-	echo "hitp-state: unavailable"	
-	exit 1
+	echo "hitp-state: unavailable"
 fi
-
-echo "hitp-title: Department : $department"
-echo "hitp-state: ok"
 
 exit 0
