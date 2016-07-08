605954269cbab74e53b0af041099e041c9d195d1
Richard Purves <franton@users.noreply.github.com>
2016-07-08 08:33:51 +0200

    Update country_info.sh
    Reworked to read info from a pre-prepared file rather than interrogate the API.

--- country_info.sh
+++ country_info.sh
@@ -2,33 +2,19 @@
 
 # Script to report current JSS building to Hello-IT
 
-# Set up variables here
-
-apiuser="apiusr"
-apipass="apiusrpw"
+buildfile="/usr/local/cs/misc/building"
 
 echo "hitp-enabled: YES"
 echo "hitp-hidden: NO"
 
-jssurl=$( defaults read /Library/Preferences/com.jamfsoftware.jamf "jss_url" 2> /dev/null )
-
-if [ "$jssurl" = "" ];
+if [ -e "$buildfile" ];
 then
-	echo "hitp-title: Not Enrolled"
-	echo "hitp-state: error"
-	exit 1
+	building=$( cat "$deptfile" )
+	echo "hitp-title: Country : $building"
+	echo "hitp-state: ok"
+else
+	echo "hitp-title: Missing Country Info"
+	echo "hitp-state: unavailable"
 fi
-
-building=$(curl -H "Accept: application/xml" -s -u "$apiuser:$apipass" "${jssurl}JSSResource/computers/udid/$udid/subset/location" | xpath "//computer/location/building/text()" 2> /dev/null)
-
-if [ "$building" = "" ];
-then
-	echo "hitp-title: Missing Building Info"
-	echo "hitp-state: unavailable"	
-	exit 1
-fi
-
-echo "hitp-title: Building : $building"
-echo "hitp-state: ok"
 
 exit 0
